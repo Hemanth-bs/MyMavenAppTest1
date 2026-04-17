4beccf863a08b93788b9252342d772b5d9c82671
Hemanth-bs <hemanthbyrareddy@gmail.com>
2026-04-17 14:22:38 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Name:Hemanth B" );
+        System.out.println( "USN:1BI23CS094" );
+    }
+}
